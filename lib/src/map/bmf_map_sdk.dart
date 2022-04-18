0b4e00ab36106d5b23d94c89ffb7c4da3c1e5907
CyJaySong <SSJloveYOU970614>
2022-04-18 11:05:16 +0800

    fix code

--- lib/src/map/bmf_map_sdk.dart
+++ lib/src/map/bmf_map_sdk.dart
@@ -13,7 +13,7 @@ class BMFMapSDK {
   ///
   /// apiKey 百度地图开放平台申请的ak
   /// coordType 坐标类型 目前不支持将全局坐标类型设置为WGS84
-  static void setApiKeyAndCoordType(
+  static Future<void> setApiKeyAndCoordType(
       String apiKey, BMF_COORD_TYPE coordType) async {
     try {
       await MethodChannel('flutter_bmfbase').invokeMethod(
@@ -26,7 +26,7 @@ class BMFMapSDK {
   /// 设置地图坐标类型
   ///
   /// coordType 坐标类型，目前不支持将全局坐标类型设置为WGS84
-  static void setCoordType(BMF_COORD_TYPE coordType) async {
+  static Future<void> setCoordType(BMF_COORD_TYPE coordType) async {
     try {
       await MethodChannel('flutter_bmfbase')
           .invokeMethod(kSetAPIKey, {'BMF_COORD_TYPE': coordType.index});
@@ -41,7 +41,7 @@ class BMFMapSDK {
   ///
   /// 隐私政策官网链接：https://lbsyun.baidu.com/index.php?title=openprivacy
   /// isAgree 用户是否同意SDK隐私协议
-  static void setAgreePrivacy(bool isAgree) async {
+  static Future<void> setAgreePrivacy(bool isAgree) async {
     try {
       await MethodChannel('flutter_bmfbase')
           .invokeMethod(kSetPrivacyAPIKey, {'isAgree': isAgree});
